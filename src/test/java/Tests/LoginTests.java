c4b66494c30927880a7317d61fccb2506ed81f14
Nkosi <njceles@gmail.com>
2025-06-07 23:51:24 +0200

    created Application constants

--- src/test/java/Tests/LoginTests.java
+++ src/test/java/Tests/LoginTests.java
@@ -10,6 +10,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import static Constants.ApplicationConstants.Login_Page_Title;
+
 public class LoginTests extends BaseTest {
 
 
@@ -18,7 +20,7 @@ public class LoginTests extends BaseTest {
     @Test
     public void login() {
         String title = loginPage.getLandingPageTile();
-        Assert.assertEquals(title, "Swag Labs");
+        Assert.assertEquals(title, Login_Page_Title, "Title does not match");
     }
 
 
